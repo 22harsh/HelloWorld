43076115d4ae376e8fda1447cc133b126fe0a09a
22harsh <harsh2210singh@gmail.com>
2021-08-17 14:29:07 +0530

    New commit

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -1,7 +1,16 @@
+package com.coveros.demo.helloworld;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 // Your First Program
+// Making new changes to the code 
 
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now(); 
+        System.out.println(dtf.format(now));
+	
     }
 }
